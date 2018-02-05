13f02f415dc53b22e4f0d2b5d2e323ea1e5c0547
YunHo <shdbsghxg@gmail.com>
2018-02-05 13:01:13 +0900

    blank, null, choices

---
+++ django/myapp/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Musician, Album, Person
+
+admin.site.register(Musician)
+admin.site.register(Album)
+admin.site.register(Person)
